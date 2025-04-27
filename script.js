4a8565fd68ac2f1e16a26152e81ea411fb96209c
Gabriel <gabriel.bfnds10@gmail.com>
2025-04-27 12:59:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Iniciando a biblioteca AOS
+AOS.init();
+
+// Sticky Navbar Effect
+window.onscroll = function() {
+  stickyHeader();
+};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyHeader() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+}
