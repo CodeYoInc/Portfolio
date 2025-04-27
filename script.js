130068893fc04a23bd161ba470b17f1fa2d0eacb
Gabriel <gabriel.bfnds10@gmail.com>
2025-04-27 17:49:58 -0300

    mudanças visuais

--- script.js
+++ script.js
@@ -17,3 +17,26 @@ function stickyHeader() {
   }
 }
 
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+  body.classList.toggle('light-theme');
+
+  if (body.classList.contains('light-theme')) {
+    body.classList.replace('bg-gradient-to-br', 'bg-gradient-to-tr');
+    body.classList.replace('from-gray-900', 'from-white');
+    body.classList.replace('via-gray-800', 'via-gray-200');
+    body.classList.replace('to-gray-900', 'to-white');
+    body.classList.replace('text-white', 'text-gray-900');
+    themeIcon.textContent = '☀️';
+  } else {
+    body.classList.replace('bg-gradient-to-tr', 'bg-gradient-to-br');
+    body.classList.replace('from-white', 'from-gray-900');
+    body.classList.replace('via-gray-200', 'via-gray-800');
+    body.classList.replace('to-white', 'to-gray-900');
+    body.classList.replace('text-gray-900', 'text-white');
+    themeIcon.textContent = '🌙';
+  }
+});
